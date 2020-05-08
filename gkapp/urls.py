3f1d5a671ee2fc150515310b6a00b2f7b448f862
unclebay143 <ayodelesamueladebayo143@gmail.com>
2020-05-08 01:04:41 -0700

    issue with modal  image gallery

--- gkapp/urls.py
+++ gkapp/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import Index, Cakes, Link, Contact, Store, Upload
+from .views import Index, Cakes, Link, Contact, Store, Upload, Fond
 urlpatterns = [
     path('', Index, name='index'),
     path('cakes', Cakes, name='cakes'),
     path('links', Link, name='links'),
     path('contact/', Contact, name='contact'),
     path('store', Store, name='store'),
-    path('upload', Upload, name='upload')
+    path('upload', Upload, name='upload'),
+    path('fond', Fond, name='fond')
 
 ]
 
--- gkapp/views.py
+++ gkapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Cake, PostImage
+from .models import *
 
 # Create your views here.
 
@@ -11,17 +11,18 @@ def Index(request):
 
 
 def  Cakes(request):
-    post = Cake.objects.all()
+    post = Cake.objects
     return render(request, 'cakes.html', {"post":post})
 
 def Contact(request):
     return render(request, 'contact.html')
 
 def Store(request):
+
     return render(request, 'store.html')
 
 def Upload(request):
-    post = get_object_or_404(Cake)
-    return render(request, 'upload.html',{
-        'post':post,        
-    })
+    return render(request, 'upload.html')
+
+def Fond(request):
+    return render(request, 'fond.html')
